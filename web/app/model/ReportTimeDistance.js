1debefdc0212fa6438353a6243839608dd4188b8
RafaelMiquelino <rafaelmiquelino@gmail.com>
2019-10-23 22:24:21 -0300

    Update copyright

--- web/app/model/ReportTimeDistance.js
+++ web/app/model/ReportTimeDistance.js
@@ -1,6 +1,7 @@
 /*
  * Copyright 2016 Anton Tananaev (anton@traccar.org)
  * Copyright 2016 Andrey Kunitsyn (andrey@traccar.org)
+ * Copyright 2019 Rafael Miquelino (rafaelmiquelino@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

7e43d3509440b07951347bf18dc33fcf4f7ee90f
RafaelMiquelino <rafaelmiquelino@gmail.com>
2020-08-12 14:56:11 -0300

    Removed Routes and Chart reports

--- web/app/store/ReportTypes.js
+++ web/app/store/ReportTypes.js
@@ -19,23 +19,30 @@ Ext.define('Traccar.store.ReportTypes', {
     extend: 'Ext.data.Store',
     fields: ['key', 'name'],
 
-    data: [{
-        key: 'route',
-        name: Strings.reportRoute
-    }, {
+    data: [
+    // {
+    //     key: 'route',
+    //     name: Strings.reportRoute
+    // },
+    {
         key: 'events',
         name: Strings.reportEvents
-    }, {
+    },
+    {
         key: 'trips',
         name: Strings.reportTrips
-    }, {
+    },
+    {
         key: 'stops',
         name: Strings.reportStops
-    }, {
+    },
+    {
         key: 'summary',
         name: Strings.reportSummary
-    }, {
-        key: 'chart',
-        name: Strings.reportChart
-    }]
+    },
+    // {
+    //     key: 'chart',
+    //     name: Strings.reportChart
+    // },
+    ]
 });
